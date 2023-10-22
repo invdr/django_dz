127494e2c46850f8b99c851406291efa703cc017
LQP <homepc@mail.ru>
2023-10-22 20:23:33 +0300

    additional task: done

--- catalog/views.py
+++ catalog/views.py
@@ -7,4 +7,9 @@ def get_index(request):
 
 
 def get_contacts(request):
+    if request.method == 'POST':
+        name = request.POST.get('name')
+        phone = request.POST.get('phone')
+        message = request.POST.get('message')
+        print(f'Новое сообщение от {name}({phone}): {message}')
     return render(request, 'catalog/contacts.html')
