4ad841e715be4f286956fdc292bd69104cc5a609
LQP <homepc@mail.ru>
2023-10-22 20:06:15 +0300

    task 3-4: add templates & setting urls

--- catalog/views.py
+++ catalog/views.py
@@ -1,10 +1,10 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 
+
 def get_index(request):
-    # return render(request, 'catalog/index.html')
-    return HttpResponse("Index page")
+    return render(request, 'catalog/index.html')
+
 
 def get_contacts(request):
-    # return render(request, 'catalog/contacts.html')
-    return HttpResponse("Contacts page")
+    return render(request, 'catalog/contacts.html')
