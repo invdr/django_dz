2081b71292c862ab4c0708a89f569cbfb945b524
Ibrahim WORK <work@mail.ri>
2023-10-26 16:13:24 +0300

    dz 20.1: task 2-3 done

--- catalog/models.py
+++ catalog/models.py
@@ -1,3 +1,34 @@
 from django.db import models
 
-# Create your models here.
+NULLABLE = {"null": True, "blank": True}
+
+
+class Category(models.Model):
+    name = models.CharField(max_length="100", verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+
+    def __str__(self):
+        return f"Категория: {self.name}"
+
+    class Meta:
+        verbose_name = "категория"
+        verbose_name_plural = "категории"
+        ordering = ('name',)
+
+
+class Product(models.Model):
+    name = models.CharField(max_length=100, verbose_name="Наименование")
+    description = models.TextField(verbose_name="Описание")
+    image = models.ImageField(upload_to="product_images/", verbose_name="Превью", **NULLABLE)
+    category_id = models.ForeignKey(Category, on_delete=models.CASCADE, verbose_name="ID категории")
+    price = models.IntegerField(default=0, verbose_name="Цена")
+    created_at = models.DateTimeField(verbose_name="Дата создания")
+    last_change = models.DateTimeField(verbose_name="Дата изменения")
+
+    def __str__(self):
+        return f"{self.name} ({self.category_id.name})"
+
+    class Meta:
+        verbose_name = "продукт"
+        verbose_name_plural = "продукты"
+        ordering = ('name', )
