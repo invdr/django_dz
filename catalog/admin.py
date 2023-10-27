dc7f87fb22dd9891989ffdf5bed2e3703c3fd0e5
Ibrahim WORK <work@mail.ri>
2023-10-27 16:55:48 +0300

    dz 20.1: task 5 register models

--- catalog/admin.py
+++ catalog/admin.py
@@ -1,3 +1,17 @@
 from django.contrib import admin
 
-# Register your models here.
+from catalog.models import Category, Product
+
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', )
+    ordering = ('name', )
+
+
+@admin.register(Product)
+class ProductAdmin(admin.ModelAdmin):
+    list_display = ('pk', 'name', 'price', 'category_id', )
+    list_filter = ('category_id', )
+    search_fields = ('name', 'description', )
+    ordering = ('name', )
