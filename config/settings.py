e7f374aa57a31cd9cc12334627917b50ae4076b3
Ibrahim WORK <work@mail.ri>
2023-11-13 17:27:38 +0300

    dz 21.1: code refactoring

--- config/settings.py
+++ config/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
 
     'catalog',
+    'blog'
 ]
 
 MIDDLEWARE = [
@@ -121,9 +122,9 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-# STATICFILES_DIRS = (
-#     BASE_DIR / 'catalog/static/',
-# )
+STATICFILES_DIRS = (
+    BASE_DIR / 'static/',
+)
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
